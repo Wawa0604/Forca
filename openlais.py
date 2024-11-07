8cf39821fa1ddc8d4a8b500ddbde24129826501b
LalaCamargo <laisoliveiracamargo@gmail.com>
2024-11-07 08:29:08 -0300

    open funcional

--- openlais.py
+++ openlais.py
@@ -1,5 +1,4 @@
 #%%
-#SÓ TESTE!!!!!!
 #Primeiro passo: importar bibliotecas
 import random
 
@@ -11,21 +10,58 @@ meuArquivo = open ('cores.txt')
 
 with open('cores.txt', 'r') as reader:
     lista = reader.readlines()
-    #print(lista)
-    #print(lista[3])
+    
 #Quarto passo: fazer ele escolher
-    # corEscolhida= random.choice(lista)
-    # print(lista)
-# Sortear um nome aleatório
 def sorteaiaCor(cor):
     return random.choice(cor)
 
-# Carregar os nomes do arquivo
-
 # Sortear um nome aleatório
-cor_sorteada = random.choice(lista)
+cor_sorteada= random.choice(lista)
 
 # Exibir o nome sorteado
 print(f'O nome sorteado foi: {cor_sorteada}')
+
+#Parte Isa:
+sorteioEscondido = cor_sorteada.replace(cor_sorteada, '_' * len(cor_sorteada))
+#contador de chances
+contador = 0
+#lista de letras ja chutadas
+letrasJaChutadas = []
+#lista das letras certas já chutadas para avisar se já chutou antes 
+#letrasCertas = []
+#verificar se os chutes são letras e apenas uma
+while True:
+
+    #enquanto a palavra estiver com letras escondidas
+    while "_" in sorteioEscondido:
+
+        # mostrar as letras escondidas
+        print (sorteioEscondido)
+        # print ("Seus chutes anteriores:", letrasErradas) #printar as q ja foram
+        # print(contador)#linha para organização
+        #imput para os chutes
+        chute = input ("Chute uma letra ")
+        
+        #verificar se os chutes são letras e apenas uma
+        if len(chute) == 1 and chute.isalpha():
+
+            #armazenas as letras já chutadas para perder chances
+            # printar as q ja foram    
+            for i in range(len(cor_sorteada)):
+                if cor_sorteada[i] == chute:
+                    sorteioEscondido = sorteioEscondido [:i] + chute + sorteioEscondido [i+1:]
+
+                #mensagem de aviso caso uma letra ja tenha ido
+                #não computar ela na contagem de vidas
+                #mensagem para quando der mais chutes q permitido
+                    if chute == cor_sorteada:
+                        break
+
+        else:
+            print("Por favor insira UMA LETRA")
+    break
+ 
+print ("Você acertou!!!")
+   
 
 # %%
